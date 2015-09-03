8998537b9fad896e253cdc5c86a5ee028b2ed1f4
ddrager <ddrager@gmail.com>
2015-09-03 13:00:55 -0400

    Test

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns
 
 urlpatterns = patterns('',
-    (r'^logdata/hem/(?P<curwatts>\d+)', 'api.views.logdata_hem'),
+    (r'^logdata/hem/(?P<curwatts>\d+)', 'api.views.logdata_hem'), # log hem data
     (r'^logdata/(?P<value>\d+)', 'api.views.logdata'), # echo back the value for testing
 )
