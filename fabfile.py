8a1881debcdd3946ae5ab9abc22d3791537f76a0
ddrager <ddrager@gmail.com>
2015-09-03 09:21:20 -0400

    Add fabric deployment scripts

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+from fabric.api import local
+
+
+def prepare_deployment(branch_name):
+    local('python manage.py test powerpleaser')
+    local('git add -p && git commit')
+    local('git checkout master && git merge ' + branch_name)
+
+
+def deploy():
+    local('git push azure master')
