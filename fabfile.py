f25e61125fce93dedf1cb6de027508d282d3eab2
ddrager <ddrager@gmail.com>
2015-09-03 12:55:56 -0400

    Remove code commit from fabric

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,6 @@ from fabric.api import local
 
 def prepare_deployment(branch_name):
     local('python manage.py test powerpleaser')
-    local('git add -p && git commit')
-    local('git checkout master && git merge ' + branch_name)
 
 
 def deploy():
